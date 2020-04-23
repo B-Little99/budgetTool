08d974604fd2207b9b979378df0675da7ebdc56e
Brandon Little <brandonlittle1999@hotmail.com>
2020-04-23 10:55:45 +0100

    Added the ability for users to update a basic pre-determined bill list and include the costs.

--- budgetTool.py
+++ budgetTool.py
@@ -14,10 +14,36 @@ stringKeys = separator.join(basicBills)
 
 print("Welcome to this simple monthly budget tool! Let's start with your monthly post-tax income:")
 monthlyIncome = int(input())
-print("There are several basic 'bills' we want to include by default. These are: " + stringKeys + ".")
+print("There are several basic 'bills' we want to include by default.")
+
+
+loop = True
+
+while loop == True:
+    print("Please write out one of the bills you would like to update from one of the following categories: " + stringKeys + ".")
+    billKey = input()
+    if billKey.lower() in basicBills:
+        print("Please input the cost of your " + str(billKey.lower()) + " in GBP:")    
+    else: 
+        print("Oops! It looks like what you entered is not on the list!")
+    
+    billCost = input()
+
+    basicBills[billKey.lower()] = billCost
+    print("Now your " + str(billKey.lower()) + " costs £" + str(billCost) + " per month!")
+
+    print("Would you like to add another cost?")
+    userDecision = input()
+    if userDecision.lower() == "yes" or userDecision.lower() == "y":
+        loop = True
+    else:
+        loop = False
+
+
+# if breakdownDecision.lower == "yes" or breakdownDecision.lower == "y":
 
 
 # Need a expenses dictionary. One can be neccessities and the other can be non-n
 # Add to each dictionary using a loop asking if they would like to add another in a while loop
 
-
+# Put them in separate lists and then use indexing to match them up in a loop
